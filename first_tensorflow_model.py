fefe63e2f2c3b7dfb413480a7cc19f68a927f907
josh flori <josh.flori@gatewayblend.com>
2019-04-10 15:04:48 -0500

    Update first_tensorflow_model.py

--- first_tensorflow_model.py
+++ first_tensorflow_model.py
@@ -1,56 +1,54 @@
 # The purpose of this script is to create a tensorflow model on the same data as my hand-crafted network.
 from __future__ import absolute_import, division, print_function
-
-# TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
-
-# Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
-
+import os
+import imageio
+import pandas as pd
+from sklearn.model_selection import train_test_split
 print(tf.__version__)
 
 
-fashion_mnist = keras.datasets.fashion_mnist
+##################
+#      PATH      #
+##################
+path='/users/josh.flori/desktop/images/'
+m=len([i for i in os.listdir(path) if 'jpg' in i])
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+##################
+#    load x/y    #
+##################
+X = np.array([imageio.imread(path+str(i)+'.jpg') for i in range(1,m+1)])          # So things to note here... mostly just... os.listdir returns some .DS_Store bullshit, so we sorted the list and it pops out in front...  then we get everything BUT that. It reads the matrix the wrong way so we flip it so that each column is an entire image, which I think is what we want. # 208 must be the total count of images + 1 for python to do it's thing
+Y=np.array(pd.read_csv('/users/josh.flori/desktop/Y.csv')['Y'].values.tolist())
+X.shape       # X.shape[1], Y.shape[1] should both be equal to m, or the number of training examples
+Y.shape
 
-train_images.shape
-len(train_labels)
-train_labels
-test_images.shape
+
+##########################
+#    STANDARDIZE DATA    #
+##########################
+X = X/255
+
+##########################
+#   ASSERTION CHECKING   #
+##########################
+assert(X.shape[0]==m)
+assert(Y.shape[0]==m)
+
 
 plt.figure()
-plt.imshow(train_images[0])
+plt.imshow(X[0])
+plt.show()
 plt.colorbar()
 plt.grid(False)
 plt.show()
 
-train_images = train_images / 255.0
-
-test_images = test_images / 255.0
-
-
-
-
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
-
-
 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Flatten(input_shape=(56, 100)),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
@@ -61,7 +59,16 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 
-model.fit(train_images, train_labels, epochs=15)
+# fucking sklearn won't split 3 ways, but i really like how clean the following is. to split 3 ways you provide 2 arguments, which must be the index number you want to split at, so out of m elements, you want to split at m*.8, or whatever
+np.random.seed(0)
+np.random.shuffle(X)
+np.random.shuffle(Y)
+train_X,dev_X,test_X = np.split(X, [int(.8*X.shape[0]), int(.9*X.shape[0])])
+train_Y,dev_Y,test_Y = np.split(Y, [int(.8*X.shape[0]), int(.9*X.shape[0])])
+
+
+# FIT THE MODEL
+model.fit(train_X, train_Y, epochs=15)
 
 
 test_loss, test_acc = model.evaluate(test_images, test_labels)
